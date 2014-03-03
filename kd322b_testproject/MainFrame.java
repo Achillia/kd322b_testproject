2771a99f0e624737dc170a3aa143970723730347
tor <tor-moller@hotmail.com>
2014-03-03 10:59:21 +0100

    Ändrat...

--- kd322b_testproject/MainFrame.java
+++ kd322b_testproject/MainFrame.java
@@ -31,6 +31,7 @@ public class MainFrame extends JFrame {
 	 */
 	public MainFrame() {
 		//Hejhej
+		System.out.println("Hjehej");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
